4709d9b8078559b739feaa237cccf822a692c8bf
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-08-30 15:16:21 +0800

    Commiting

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,3 +2,11 @@
 """
     Lets have a small description of the project
 """
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Return page indexed points """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -2,3 +2,51 @@
 """
     Lets have a small description of the project
 """
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ Return page indexed points """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Get a page items with the page size """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start, end = index_range(page=page, page_size=page_size)
+        data = []
+        with open(file="data_file.csv") as file:
+            line = file.readline().strip()
+            i = 1
+            while line != "":
+                line = file.readline().strip()
+                if i <= start:
+                    i += 1
+                elif start <= i < end + 1:
+                    data.append(line.split(sep=","))
+                    i += 1
+        return data
