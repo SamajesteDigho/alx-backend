d20b0da1bb279271e38c0b6ae5f004e1dd245128
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-05 22:29:33 +0800

    Commiting exo 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,15 +9,15 @@ class BasicCache(BaseCaching):
     """ BasicCache class inheriting from BaseCaching """
     def __init__(self):
         super().__init__()
-    
+
     def put(self, key, item):
         """ Putting data into cash """
         if key is not None and item is not None:
             self.cache_data[key] = item
-    
+
     def get(self, key):
         """ Getting data with associated to given key """
         try:
             return self.cache_data[key]
-        except:
+        except Exception:
             return None
