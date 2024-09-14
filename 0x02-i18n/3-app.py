e31c1f8fc809a7e384086cc3d759b69accb3064d
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-15 06:41:23 +0800

    Commiting

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -2,3 +2,28 @@
 """
 Here the module description file
 """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """ Babel configuraion class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+def get_locale():
+    """ Get the local """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """ Here the initial root target """
+    return render_template("3-index.html")
