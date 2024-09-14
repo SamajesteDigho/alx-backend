4f20ef75a5038abb7cdd436912ba0c1bcd16a512
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-15 06:57:05 +0800

    Comiting

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -2,3 +2,32 @@
 """
 Here the module description file
 """
+from flask import Flask, render_template, request
+from flask_babel import Babel, localeselector
+
+
+class Config:
+    """ Babel configuraion class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@localeselector
+def get_locale():
+    """ Get the local """
+    lang = request.args.get('locale')
+    print("Language")
+    if lang:
+        return lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route("/")
+def index():
+    """ Here the initial root target """
+    return render_template("4-index.html")
