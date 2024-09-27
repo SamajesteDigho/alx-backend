497e3f2795c7af2bcd5fc378325ef1a758c9063c
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-27 09:22:02 +0800

    Commiting

--- 0x02-i18n/app.py
+++ 0x02-i18n/app.py
@@ -6,7 +6,6 @@ from datetime import datetime
 from typing import Dict, Union
 from flask import Flask, render_template, request, g
 from flask_babel import Babel,  format_datetime
-import flask_babel as babel
 import pytz
 
 
@@ -15,6 +14,11 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @babel.localeselector
@@ -51,9 +55,6 @@ def get_timezone():
         return app.config['BABEL_DEFAULT_TIMEZONE']
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-babel = Babel(app, locale_selector=get_locale, timezone_selector=get_timezone)
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
