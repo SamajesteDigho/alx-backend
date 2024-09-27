a5929422fecbb008d93686928dd1a37051dfc811
samajestedigho <dighodonmezatankeujordan@gmsil.com>
2024-09-27 12:19:59 +0800

    Commiting

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -13,14 +13,15 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-def get_locale():
-    """ Get the local """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Get the local """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route("/")
